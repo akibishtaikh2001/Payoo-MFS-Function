946b81940e39dd868951725b70d4d408628785ca
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-26 22:17:43 +0600

    Recap Add money layout handler and reasons for shared code

---
+++ JS/addMoney.js
@@ -0,0 +1,19 @@
+/**
+ * How to get a number from an input field 
+ * 1. create a variable
+ * 2. right side document.getElementByid(id)
+ * 3. .value
+ * 4. parseFloat
+ */
+
+console.log('add money loaded');
+
+document.getElementById('btn_add_money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    
+    console.log('add money button clicked');
+    // const addMoney = document.getElementById('input_add_money').value;
+    // const addMoneyNumber = parseFloat(addMoney);
+
+})
